204e3ba5437d529b7381295aa36f9a36b01b7efc
unthinkable-lap <priyanshu.bhatt@unthinkable.co>
2024-01-22 17:38:28 +0530

    changed project

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
   }
   backend "remote" {
         # The name of your Terraform Cloud organization.
-         organization = "testing-action"
+         organization = "my-error-org"
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-          name = "IAC-workflow"
+          name = "error-testing"
         }
       }
 }
