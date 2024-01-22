2e5b20407763a06d81f6a97cd01e6f64d3c16ff6
unthinkable-lap <priyanshu.bhatt@unthinkable.co>
2024-01-22 17:18:47 +0530

    added api

--- main.tf
+++ main.tf
@@ -5,10 +5,18 @@ terraform {
       version = "5.33.0"
     }
   }
+  backend "remote" {
+        # The name of your Terraform Cloud organization.
+         organization = "testing-action"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "IAC-workflow"
+        }
+      }
 }
 
 provider "aws" {
-  # Configuration options
-  profile = "terraform"
+    region = "ap-south-1"
   
 }
